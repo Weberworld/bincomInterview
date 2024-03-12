76fddc6a3a967b42488051fd57e0a08625739260
Weberworld <omotosoboluwatife23@gmail.com>
2024-03-12 19:11:08 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,27 @@
+## Bincom Online Interview Test
+
+### Python Developer (Intership) exercise Using Django
+
+#
+#
+### Installation
+> - Create a virtual environment
+>
+> ```python -m venv myvenv```
+> - Install the project requirement library
+> 
+> ```pip install -r requirments.txt```
+> - Input your MySQL credentials in env file
+```
+    DATABASE_NAME=YOUR_DATABASE_NAME
+    DATABASE_USER=YOUR_DATABASE_USER
+    DATABASE_PSWD=YOUR_DATABASE_PSWD
+    DATABASE_HOST=YOUR_DATABASE_HOST
+    DATABASE_PORT=YOUR_DATABASE_HOST
+```
+> - Create the MySQL db
+> 
+> ```python create_db.py```
+> - Run the server
+> 
+> ``` python manage.py runserver```
--- create_db.py
+++ create_db.py
@@ -1,9 +1,11 @@
+import os
+
 import mysql.connector
 try:
     mydb = mysql.connector.connect(
-      host="localhost",
-      user="Weber",
-      password="manuels2385."
+      host=os.getenv("DATABASE_HOST"),
+      user=os.getenv("DATABASE_USER"),
+      password=os.getenv("DATABASE_PSWD")
     )
 
     mycursor = mydb.cursor()
@@ -15,22 +17,15 @@ except Exception:
     pass
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="Weber",
-  password="manuels2385.",
-db="polls"
+    host=os.getenv("DATABASE_HOST"),
+    user=os.getenv("DATABASE_USER"),
+    password=os.getenv("DATABASE_PSWD"),
+    db=os.getenv("DATABASE_NAME")
 )
 
 mycursor = mydb.cursor()
 mycursor.execute(open("bincom_test.sql").read())
 mycursor.close()
 
-
-# mycursor.execute("SELECT * FROM `agentname`")
-#
-# myresult = mycursor.fetchall()
-#
-# for x in myresult:
-#   print(x)
 
 
